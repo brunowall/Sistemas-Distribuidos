6214ef3306bd9b5e27b0106be127f4a9006eb86a
brunowall <brunowallison.fernandes@gmail.com>
2017-11-21 23:15:21 -0300

    adicionado algumas coisas

--- TrabalhoSD3/Q2/calculadora.py
+++ TrabalhoSD3/Q2/calculadora.py
@@ -1,18 +1,15 @@
 import socket
 import messages_pb2
 import threading
-
+import message_pb2
 def main():
     ip_service = "" # porta em que o servico esta rodando
-    port_service = 8010
+    port_service = 8050
     desc = threading.Thread(target=descoberta,args=[ip_service,port_service])
+    net = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    net.bind((ip_service,port_service))
     desc.start()
-
-    net_tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # esse socket recebe as operacoes da calculadora
-    net_tcp.bind((ip_service,port_service))
-    net_tcp.listen(5)
-    con,addr =  net_tcp.accept()
-    print("Connectado a um cliente")
+    calculadora(net)
 
 def descoberta(ip_servico,porta_servico):
     net = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket recebe as solicitacoes de descoberta
@@ -26,4 +23,28 @@ def descoberta(ip_servico,porta_servico):
             response.ip = ip_servico
             response.port= porta_servico
             net.sendto(response.SerializeToString(),ip)
+
+def calculadora(net):
+
+    while True:
+        print("Escutando requisicao...")
+        operation = message_pb2.Request()
+        resposta = message_pb2.Reply()
+        data, addr = net.recvfrom(1024) #receive 1024 bytes
+        operation.ParseFromString(data)
+        resposta.res = calcula(operation.n1,operation.op,operation.n2)
+        net.sendto(resposta.SerializeToString(),addr)
+        print ("resultado enviado para: ",addr)
+
+def calcula(num1,op,num2):
+        if op == message_pb2.Request.SOM:
+            return num1+num2
+        elif op == message_pb2.Request.SUB :
+            return num1-num2
+        elif op == message_pb2.Request.MUL:
+            return num1*num2
+        elif op == message_pb2.Request.DIV:
+            return num1/num2
+
+
 main()
--- TrabalhoSD3/Q2/cliente.py
+++ TrabalhoSD3/Q2/cliente.py
@@ -1,18 +1,48 @@
 import socket
 import messages_pb2
-
+import message_pb2
 def main():
     net = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     request = messages_pb2.Request()
     response = messages_pb2.Reply()
-    request.op =  messages_pb2.CALCULADORA
     net.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     net.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    net.sendto(request.SerializeToString(),("255.255.255.255",8000))
-    data, ip = net.recvfrom(2048)
-    response.ParseFromString(data)
-    print("Calculadora encontrada ")
-    net_tcp  = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    net_tcp.connect((response.ip,response.port))
-    
+
+    flag = True
+    while flag:
+        op = int(raw_input("Digite 0 para impressao, 1  Calculadora, 2 Som, 3 projetor"))
+        request.op = op
+        net.sendto(request.SerializeToString(),("255.255.255.255",8000))
+        net.settimeout(4) # seta o tempo de timeout para 4 segundos
+        try:
+            data, ip = net.recvfrom(2048)
+            response.ParseFromString(data)
+            flag = False # para o laco se encontrar o dispositivo
+        except:
+            print("Dispositivos nao encontrado")
+            continue
+
+    print("Encontrado!")
+    tupla = (response.ip,response.port)
+
+    while True:
+        operation = message_pb2.Request()
+        operation.n1 = float(raw_input("Digite o primeiro numero"))
+        op = raw_input("digite a operacao")
+        operation.n2 = float(raw_input("Digite o segundo numero"))
+        resposta = message_pb2.Reply()
+        if op == "+":
+            operation.op=message_pb2.Request.SOM
+        elif op == "-" :
+            operation.op=message_pb2.Request.SUB
+        elif op == "*":
+            operation.op=message_pb2.Request.MUL
+        elif op == "/":
+            operation.op=message_pb2.Request.DIV
+
+        net.sendto(operation.SerializeToString(),tupla)
+        data, add = net.recvfrom(1024)
+        resposta.ParseFromString(data)
+        print (resposta.res);
+
 main()
